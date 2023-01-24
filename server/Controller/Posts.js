c998c0d192baaa6a205fcfb9eedf18109cc38bed
nicolaisirghi <85614679+nicolaisirghi@users.noreply.github.com>
2023-01-24 21:21:00 +0200

    Third changes

--- server/Controller/Posts.js
+++ server/Controller/Posts.js
@@ -1,86 +1,118 @@
-const Questions = require("../Models/Questions")
-const Answers = require("../Models/Answers")
-const Users = require("../Models/Users")
-module.exports.getCategories= async function(req,res){
-    try{
-        const posts = await Questions.find();
-        const categories = posts.map(el=>el.category)
-        const uniqueCategories  = categories.filter((item,index)=>categories.indexOf(item)===index)
-        res.status(200).json({categories:uniqueCategories})
-    }
-    catch (e){
-        res.status(400).send(e)
-    }
-}
+const Questions = require("../Models/Questions");
+const Answers = require("../Models/Answers");
+const Users = require("../Models/Users");
+module.exports.getCategories = async function (req, res) {
+  try {
+    const posts = await Questions.find();
+    const categories = posts.map((el) => el.category);
+    const uniqueCategories = categories.filter(
+      (item, index) => categories.indexOf(item) === index
+    );
+    res.status(200).json({ categories: uniqueCategories });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
 module.exports.getQuestions = async function (req, res) {
-    try {
-        const category = req.params.category;
-        const questions = await Questions.find({category:category}, {_v: 0})
-        const comments = []
-        for (let i = 0; i < questions.length; i++) {
-            const answers = await Answers.find({answeredTo: questions[i]._id})
-            const user = await Users.findOne({_id:questions[i].user},{_id:0,email:0,password:0})
-            comments.push({questionInfo: questions[i], answerInfo: answers,userInfo:user})
-        }
-        res.status(200).json(comments)
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
+  try {
+    const category = req.params.category;
+    let page, itemsCount;
+    console.log("Query parameters : ",req.query)
+    if (req.query.page) {
+      page = req.query.page;
+    } else {
+      page = 1;
     }
-}
+    if (req.query.itemsCount) {
+      itemsCount = req.query.itemsCount;
+    } else {
+      itemsCount = 5;
+    }
+    
+
+    const questions = await Questions.find({ category: category }, { _v: 0 });
+    const comments = [];
+    for (let i = 0; i < questions.length; i++) {
+      const answers = await Answers.find({ answeredTo: questions[i]._id });
+      const user = await Users.findOne(
+        { _id: questions[i].user },
+        { _id: 0, email: 0, password: 0 }
+      );
+      comments.push({
+        questionInfo: questions[i],
+        answerInfo: answers,
+        userInfo: user,
+      });
+    }
+    const itemsFromCollectionSize = comments.length;
+    const totalPages = Math.ceil(itemsFromCollectionSize / itemsCount);
+    let newArr;
+    if (page <= totalPages) { newArr = comments.slice(page - 1, itemsCount); }
+    else if (page>totalPages){
+      newArr = comments.slice(totalPages - 1 , itemsCount)
+    }
+    res.status(200).json({ newArr, itemsCount: itemsFromCollectionSize });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+};
 module.exports.addQuestion = async function (req, res) {
-    try {
-        const questionDocument = {...req.body.questionInfo, date: new Date()};
-        const category = req.params.category;
-        const questionFromCollection = await Questions.findOne({question:questionDocument.question})
-        if(!questionFromCollection) {
-            await (new Questions({...questionDocument,category})).save();
-            res.status(200).send("Question added with success!")
-        }
-        else {
-            res.status(400).send("This question is already asked!")
-        }
-    } catch (e) {
-        res.status(400).send(e)
+  try {
+    const questionDocument = { ...req.body.questionInfo, date: new Date() };
+    const category = req.params.category;
+    const questionFromCollection = await Questions.findOne({
+      question: questionDocument.question,
+    });
+    if (!questionFromCollection) {
+      await new Questions({ ...questionDocument, category }).save();
+      res.status(200).send("Question added with success!");
+    } else {
+      res.status(400).send("This question is already asked!");
     }
-
-}
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 module.exports.addAnswer = async function (req, res) {
-    const { answerInfo} = req.body;
+  const { answerInfo } = req.body;
 
-    console.log(answerInfo)
-    const questionCandidate = await Questions.findOne({_id:answerInfo.answeredTo});
-    console.log(questionCandidate)
-    if (questionCandidate) {
-        const answerDocument = {...answerInfo, date: new Date()};
-        try {
+  console.log(answerInfo);
+  const questionCandidate = await Questions.findOne({
+    _id: answerInfo.answeredTo,
+  });
+  console.log(questionCandidate);
+  if (questionCandidate) {
+    const answerDocument = { ...answerInfo, date: new Date() };
+    try {
+      await new Answers(answerDocument).save();
 
-            await (new Answers(answerDocument)).save()
-
-            res.status(200).send(`Answered to question ${questionCandidate.question} with success!`)
-        } catch (e) {
-            res.status(400).send(e)
-        }
+      res
+        .status(200)
+        .send(
+          `Answered to question ${questionCandidate.question} with success!`
+        );
+    } catch (e) {
+      res.status(400).send(e);
     }
-    else{
-        res.status(404).send("Question not found")
-    }
-}
-
+  } else {
+    res.status(404).send("Question not found");
+  }
+};
 
 module.exports.getCommentsByQuestion = async function (req, res) {
-    const questionID = req.query.questionID;
-    try {
-        const question = await Questions.findOne({_id: questionID})
-        if (question) {
-            const answers = await Answers.find({answeredTo: questionID})
-            const comment = {questionInfo:question, answerInfo:answers}
-            res.status(200).send(comment)
-        } else {
-            res.status(400).send(questionID)
-        }
-    } catch (e) {
-        res.status(404).send(e)
-
+  const questionID = req.query.questionID;
+  try {
+    const question = await Questions.findOne({ _id: questionID });
+    if (question) {
+      const answers = await Answers.find({ answeredTo: questionID });
+      const comment = { questionInfo: question, answerInfo: answers };
+      res.status(200).send(comment);
+    } else {
+      res.status(400).send(questionID);
     }
-}
+  } catch (e) {
+    res.status(404).send(e);
+  }
+};
