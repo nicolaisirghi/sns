2009994d45096ed43b1f5a126f7a8d69c6ae7cf0
nicolaisirghi <nicolai.sirghi@gmail.com>
2023-02-08 15:44:15 +0200

    Initializa logs

--- server/index.js
+++ server/index.js
@@ -10,32 +10,50 @@ const app = express()
 const session = require("express-session")
 const errorHandler = require("./Middleware/errorHandlerMiddleware")
 const PORT = process.env.PORT || 5000
-app.get('/', (_, res) => {
-    res.status(200).json({
-        name: 'BrainWaveAPI',
-        version: "1.0.0",
-        message: "This is the API for the web-application BrainWave",
-        author: "Sirghi Nicolae",
-    })
-})
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-app.use(cors())
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
-app.use("/posts", postsRouter)
-app.use("/auth", authRouter)
-app.use(errorHandler)
 const start = async () => {
     try {
+        const server = app.listen(PORT, () =>
+            console.log(`[Express] Server has been started on ${PORT}`));
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
-        console.log("Connected to mongo ")
+        console.log("[Mongo] Connected to mongo ")
+        const socketIO = require('socket.io')(server, {
+            cors: {
+                origin: "http://localhost:3000"
+            }
+        });
+
+        //Add this before the app.get() block
+        socketIO.on('connection', (socket) => {
+            console.log(`⚡: ${socket.id} user just connected!`);
+            socket.on('disconnect', () => {
+                console.log(`🔥: ${socket.id} user just disconnected!`);
+            });
+        });
+        app.get('/', (_, res) => {
+            res.status(200).json({
+                name: 'BrainWaveAPI',
+                version: "1.0.0",
+                message: "This is the API for the web-application BrainWave",
+                author: "Sirghi Nicolae",
+            })
+        })
+        app.use(bp.json())
+        app.use(bp.urlencoded({ extended: true }))
+        app.use(cors())
+        app.use(session({
+            secret: 'keyboard cat', resave: true,
+            saveUninitialized: true
+        }))
+        app.use("/posts", postsRouter)
+        app.use("/auth", authRouter)
+        app.use(errorHandler)
     } catch (e) {
         console.log(e)
     }
-    app.listen(PORT, () =>
-        console.log(`Server has been started on ${PORT}`));
+
 }
 start()
---
+++ server/utils/chalk.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+module.exports = {
+    whiteText : chalk.white,
+    greenText : chalk.green,
+    orangeText : chalk.orange,
+    redText : chalk.red,
+    blueText : chalk.blue
+
+}
