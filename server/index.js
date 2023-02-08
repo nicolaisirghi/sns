d3cc6547aa09757f576db2f92bdcad41739c0c03
nicolaisirghi <85614679+nicolaisirghi@users.noreply.github.com>
2023-02-08 22:30:19 +0200

    Added logger

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import "dotenv/config"
 import mongoose from 'mongoose'
 import bp from 'body-parser'
 import cors from 'cors'
-import {router as postRouter} from './Routes/Posts.js'
-import {router as authRouter} from './Routes/Auth.js'
+import path, { dirname } from 'path'
+import { router as postRouter } from './Routes/Posts.js'
+import { router as authRouter } from './Routes/Auth.js'
 import { Server } from 'socket.io'
 import session from 'express-session'
-import {errorHandler} from './Middleware/errorHandlerMiddleware.js'
+import { errorHandler } from './Middleware/errorHandlerMiddleware.js'
 import { logger } from './utils/logger.js'
+import { fileURLToPath } from 'url'
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -16,7 +18,7 @@ const start = async () => {
     try {
         const server = app.listen(PORT, () =>
             logger.info((`[Express] Server has been started on ${PORT}`)));
-           
+
         mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB, {
             useNewUrlParser: true,
@@ -34,18 +36,17 @@ const start = async () => {
                 logger.error(`🔥: ${socket.id} user just disconnected!`);
             });
         });
-        app.use(function(req,_,next)
-        {
-            req.io =socketIO;
+        app.use(function (req, _, next) {
+            req.io = socketIO;
             next();
         })
         app.get('/', (_, res) => {
-            res.status(200).json({
-                name: 'BrainWaveAPI',
-                version: "1.0.0",
-                message: "This is the API for the web-application BrainWave",
-                author: "Sirghi Nicolae",
-            })
+            const __filename = fileURLToPath(import.meta.url)
+            const __dirname = dirname(__filename)
+            const options = {
+                root: path.join(__dirname)
+            };
+            res.sendFile('index.html',options)
         })
         app.use(bp.json())
         app.use(bp.urlencoded({ extended: true }))
--- server/utils/logger.js
+++ server/utils/logger.js
@@ -11,6 +11,17 @@ export const logger = winston.createLogger(
       align(),
       printf((info) => `[${info.timestamp}] [${info.level}]: ${info.message}`)
     ),
-    transports: [new winston.transports.Console({ format: winston.format.combine(winston.format.colorize())})],
+    transports: [new winston.transports.Console({ format: winston.format.combine(winston.format.colorize())}),
+      new (winston.transports.File)({
+        name: 'info-file',
+        filename: 'filelog-info.log',
+        level: 'info'
+      }),
+      new (winston.transports.File)({
+        name: 'error-file',
+        filename: 'filelog-error.log',
+        level: 'error'
+      })
+    ],
   }   
 )
