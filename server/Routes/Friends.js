da3491742ea470540245783eca76a0a0c87ccaa3
nicolaisirghi <85614679+nicolaisirghi@users.noreply.github.com>
2023-03-05 11:54:39 +0200

    Friend router small changes

--- server/Routes/Friends.js
+++ server/Routes/Friends.js
@@ -2,7 +2,7 @@ import express from "express";
 import { FriendControllerInstance as FriendController } from "../Controller/friendController.js";
 const router = express.Router();
 router.get("/", FriendController.getFriends);
-router.get("/request", FriendController.requestFriend);
+router.post("/request", FriendController.requestFriend);
 router.post("/add", FriendController.addFriend);
 router.delete("/remove", FriendController.removeFriend);
 export default router;
