406e9a49eccfe00451e29eacad897baf84902175
nicolaisirghi <nicolai.sirghi@gmail.com>
2023-06-11 23:51:56 +0300

    [Fix] Commented logic with mail

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -72,13 +72,13 @@ class AuthController {
     try {
       const { registrationData } = req.body;
       const userData = await authService.registration(registrationData);
-      const accessLink = uuid();
-      const verificationURL = `${process.env.API_URL}/verification/${accessLink}`;
-      await mailService.sendActivationMail(
-        userData.user.email,
-        verificationURL
-      );
-      await new MailsModel({ user: userData.user.id, link: accessLink }).save();
+      // const accessLink = uuid();
+      // const verificationURL = `${process.env.API_URL}/verification/${accessLink}`;
+      // await mailService.sendActivationMail(
+      //   userData.user.email,
+      //   verificationURL
+      // );
+      // await new MailsModel({ user: userData.user.id, link: accessLink }).save();
       req.session.refreshToken = userData.refreshToken;
       return res.json(userData);
     } catch (e) {
